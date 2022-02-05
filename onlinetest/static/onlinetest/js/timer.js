7089dd95d883927ccea138e629df8e32956d03aa
mayankt18 <mt.20u10405@btech.nitdgp.ac.in>
2022-02-05 22:34:37 +0530

    [ADD] Sticky timer

--- onlinetest/static/onlinetest/js/timer.js
+++ onlinetest/static/onlinetest/js/timer.js
@@ -2,105 +2,132 @@
 var countdown = document.getElementById("countdown");
 var timer;
 var specs = {
-    'radius': 40,
-    'centerX': 40,
-    'centerY': 40,
-    'thickness': 7,
-    'offset': -Math.PI / 2,
-    'color': 'teal',
-    'bgColor': 'white',
-    'idFont': 'small-caps 400 0.8rem Slackey',
-    'valueFont': '1.5rem Slackey',
-    'font' : 'Slackey',
-    'fontColor' : '#f77468',
+  radius: 35,
+  centerX: 35,
+  centerY: 35,
+  thickness: 7,
+  offset: -Math.PI / 2,
+  color: "teal",
+  bgColor: "white",
+  idFont: "small-caps 400 0.8rem Slackey",
+  valueFont: "1.2rem Slackey",
+  font: "Slackey",
+  fontColor: "#f77468",
 };
 var time = {
-    'millisecond': 1000,
-    'second': 60,
-    'minute': 60,
-    'hour': 24,
-}
+  millisecond: 1000,
+  second: 60,
+  minute: 60,
+  hour: 24,
+};
 var info = {};
 
-
 // canvas init
-var canvasElements = Array.prototype.slice.call(document.querySelectorAll('canvas'));
+var canvasElements = Array.prototype.slice.call(
+  document.querySelectorAll("canvas")
+);
 var canvasCtx = [];
 canvasElements.forEach(function (canvas, index) {
-    canvas.width = specs.centerX * 2;
-    canvas.height = specs.centerY * 2;
-    canvasCtx[index] = canvas.getContext('2d');
-    var name = canvas.id;
-    info[name] = { 'ctx': index, 'value': 0, 'prevValue': -1 };
+  canvas.width = specs.centerX * 2;
+  canvas.height = specs.centerY * 2;
+  canvasCtx[index] = canvas.getContext("2d");
+  var name = canvas.id;
+  info[name] = { ctx: index, value: 0, prevValue: -1 };
 });
 var canvasKeys = Object.keys(info);
 info.hours.denominator = time.hour;
 info.minutes.denominator = time.minute;
 info.seconds.denominator = time.second;
 
-
 // show remaining time
 function showRemainingTime() {
-    // calculate new values
-    var secondsLeft = init_time;
+  // calculate new values
+  var secondsLeft = init_time;
 
-    init_time -= 1;
-    if(init_time < 0){
-        init_time = 0;
+  init_time -= 1;
+  if (init_time < 0) {
+    init_time = 0;
+  }
+  // info.minutes.value = Math.floor((secondsLeft % (time.second * time.minute)) / time.second);
+  info.hours.value = Math.floor(secondsLeft / (time.second * time.second));
+
+  secondsLeft %= time.second * time.second;
+
+  info.minutes.value = Math.floor(secondsLeft / time.second);
+  info.seconds.value = Math.floor(secondsLeft % time.second);
+
+  // update changed values only
+  canvasKeys.forEach(function (key) {
+    if (info[key].value !== info[key].prevValue) {
+      draw(
+        canvasCtx[info[key].ctx],
+        info[key].value / info[key].denominator,
+        key,
+        info[key].value
+      );
+      info[key].prevValue = info[key].value;
     }
-    // info.minutes.value = Math.floor((secondsLeft % (time.second * time.minute)) / time.second);
-    info.hours.value = Math.floor(secondsLeft / (time.second * time.second))
-    
-    secondsLeft %= time.second * time.second;
-
-    info.minutes.value = Math.floor(secondsLeft / time.second);
-    info.seconds.value = Math.floor(secondsLeft % time.second);
-
-    // update changed values only
-    canvasKeys.forEach(function (key) {
-        if (info[key].value !== info[key].prevValue) {
-            draw(canvasCtx[info[key].ctx], info[key].value / info[key].denominator, key, info[key].value);
-            info[key].prevValue = info[key].value;
-        }
-    });
+  });
 }
 
 // draw function
 function draw(ctx, part, id, value) {
-    // calculate angles
-    
-    var start = specs.offset;
-    var between = 2 * Math.PI * part + specs.offset;
-    var end = 2 * Math.PI + specs.offset;
+  // calculate angles
 
-    // clear canvas
-    ctx.clearRect(0, 0, specs.centerX * 2, specs.centerY * 2);
+  var start = specs.offset;
+  var between = 2 * Math.PI * part + specs.offset;
+  var end = 2 * Math.PI + specs.offset;
 
-    // draw remaining %
-    ctx.fillStyle = specs.color;
-    ctx.beginPath();
-    ctx.arc(specs.centerX, specs.centerY, specs.radius, start, between);
-    ctx.arc(specs.centerX, specs.centerY, specs.radius - specs.thickness, between, start, true);
-    ctx.closePath();
-    ctx.fill();
+  // clear canvas
+  ctx.clearRect(0, 0, specs.centerX * 2, specs.centerY * 2);
 
-    // draw bg
-    ctx.fillStyle = specs.bgColor;
-    ctx.beginPath();
-    ctx.arc(specs.centerX, specs.centerY, specs.radius, between, end);
-    ctx.arc(specs.centerX, specs.centerY, specs.radius - specs.thickness, end, between, true);
-    ctx.closePath();
-    ctx.fill();
+  // draw remaining %
+  ctx.fillStyle = specs.color;
+  ctx.beginPath();
+  ctx.arc(specs.centerX, specs.centerY, specs.radius, start, between);
+  ctx.arc(
+    specs.centerX,
+    specs.centerY,
+    specs.radius - specs.thickness,
+    between,
+    start,
+    true
+  );
+  ctx.closePath();
+  ctx.fill();
 
-    // draw text
-    ctx.fillStyle = specs.fontColor;
-    ctx.font = specs.idFont;
-    ctx.fillText(id, specs.radius - ctx.measureText(id).width / 2, (specs.thickness+3) * 3);
-    ctx.font = specs.valueFont;
-    ctx.fillText(value, specs.radius - ctx.measureText(value).width / 2, specs.radius * 2 - specs.thickness * 3);
+  // draw bg
+  ctx.fillStyle = specs.bgColor;
+  ctx.beginPath();
+  ctx.arc(specs.centerX, specs.centerY, specs.radius, between, end);
+  ctx.arc(
+    specs.centerX,
+    specs.centerY,
+    specs.radius - specs.thickness,
+    end,
+    between,
+    true
+  );
+  ctx.closePath();
+  ctx.fill();
+
+  // draw text
+  ctx.fillStyle = specs.fontColor;
+  ctx.font = specs.idFont;
+  ctx.fillText(
+    id,
+    specs.radius - ctx.measureText(id).width / 2,
+    (specs.thickness + 3) * 3
+  );
+  ctx.font = specs.valueFont;
+  ctx.fillText(
+    value,
+    specs.radius - ctx.measureText(value).width / 2,
+    specs.radius * 2 - specs.thickness * 3
+  );
 }
 
-function startTimer(){
-    // change countdown every second
-    timer = setInterval(showRemainingTime, 1000);
+function startTimer() {
+  // change countdown every second
+  timer = setInterval(showRemainingTime, 1000);
 }
